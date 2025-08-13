8be390276085f425c0d9ea7ab58fb9047cbb5de9
Yamir mena work <ymena@pancanal.com>
2025-08-13 15:53:06 -0500

    corregir main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { useEventosStore } from './stores/eventos'
+import '@fortawesome/fontawesome-free/css/all.css';
+import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
 const pinia = createPinia()
+
 app.use(pinia)
 app.use(router)
 
-// Cargar eventos desde Firestore al iniciar
 const eventosStore = useEventosStore()
 eventosStore.cargarEventos()
-
 app.mount('#app')
